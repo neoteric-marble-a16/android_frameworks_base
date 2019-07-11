b0c36181c260f2f8c6ff67c5da7ad4b883f3e7c1
varund7726 <date.varun123@gmail.com>
2019-07-11 11:04:15 +0300

    SystemUI: Option to disable Data Disabled Indicator icon [1/2]
    Change-Id: I4e5b058d9a19070494659f52be8d95892b0eaf5d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -47,6 +47,7 @@ import android.net.TelephonyNetworkSpecifier;
 // QTI_END: 2023-07-12: Android_UI: SystemUI: Modify exclamation logic same as CR3503654 for ShadeCarrierGroupController
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.Settings;
 import android.provider.Settings.Global;
 import android.telephony.CellSignalStrength;
 import android.telephony.CellSignalStrengthCdma;
@@ -81,6 +82,7 @@ import com.android.settingslib.mobile.MobileStatusTracker.MobileStatus;
 import com.android.settingslib.mobile.MobileStatusTracker.SubscriptionDefaults;
 import com.android.settingslib.mobile.TelephonyIcons;
 import com.android.settingslib.net.SignalStrengthUtil;
+import com.android.systemui.Dependency;
 import com.android.systemui.res.R;
 import com.android.systemui.statusbar.pipeline.mobile.domain.interactor.MobileIconsInteractor;
 import com.android.systemui.statusbar.pipeline.mobile.util.MobileMappingsProxy;
@@ -89,6 +91,8 @@ import com.android.systemui.statusbar.policy.FiveGServiceClient;
 import com.android.systemui.statusbar.policy.FiveGServiceClient.FiveGServiceState;
 import com.android.systemui.statusbar.policy.FiveGServiceClient.IFiveGStateListener;
 // QTI_END: 2018-07-10: Android_UI: SystemUI: Display 5G information
+
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.CarrierConfigTracker;
 
 import java.io.PrintWriter;
@@ -103,7 +107,8 @@ import java.util.Map;
  * @deprecated Use {@link MobileIconsInteractor} instead.
  */
 @Deprecated
-public class MobileSignalController extends SignalController<MobileState, MobileIconGroup> {
+public class MobileSignalController extends SignalController<MobileState, MobileIconGroup>
+        implements TunerService.Tunable {
     private static final SimpleDateFormat SSDF = new SimpleDateFormat("MM-dd HH:mm:ss.SSS");
     private static final int STATUS_HISTORY_SIZE = 64;
     private final TelephonyManager mPhone;
@@ -117,6 +122,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     // Save entire info for logging, we only use the id.
     final SubscriptionInfo mSubscriptionInfo;
     private Map<String, MobileIconGroup> mNetworkToIconLookup;
+
+    private static final String DATA_DISABLED_ICON =
+            "system:" + Settings.System.DATA_DISABLED_ICON;
+
+    private boolean mDataDisabledIcon;
 
     private MobileIconGroup mDefaultIcons;
     private Config mConfig;
@@ -249,6 +259,20 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
             }
         };
 // QTI_END: 2023-07-12: Android_UI: SystemUI: Modify exclamation logic same as CR3503654 for ShadeCarrierGroupController
+        Dependency.get(TunerService.class).addTunable(this, DATA_DISABLED_ICON);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case DATA_DISABLED_ICON:
+                mDataDisabledIcon =
+                    TunerService.parseIntegerSwitch(newValue, true);
+                updateTelephony();
+                break;
+            default:
+                break;
+        }
     }
 
     void setConfiguration(Config config) {
@@ -802,7 +826,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         mCurrentState.roaming = isRoaming();
         if (isCarrierNetworkChangeActive()) {
             mCurrentState.iconGroup = TelephonyIcons.CARRIER_NETWORK_CHANGE;
-        } else if (isDataDisabled() && !mConfig.alwaysShowDataRatIcon) {
+        } else if (isDataDisabled() && mDataDisabledIcon) {
             if (mSubscriptionInfo.getSubscriptionId() != mDefaults.getDefaultDataSubId()) {
                 mCurrentState.iconGroup = TelephonyIcons.NOT_DEFAULT_DATA;
             } else {
--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconInteractor.kt
@@ -24,6 +24,7 @@
 // QTI_END: 2025-04-15: Android_UI: SystemUI: Readapt Mobile Icon Features For Kairos part 1
 package com.android.systemui.statusbar.pipeline.mobile.domain.interactor
 import android.content.Context
+import android.provider.Settings
 import com.android.internal.telephony.flags.Flags
 // QTI_BEGIN: 2025-04-15: Android_UI: SystemUI: Readapt Mobile Icon Features For Kairos part 1
 import android.telephony.CarrierConfigManager
@@ -39,7 +40,9 @@ import com.android.settingslib.mobile.MobileIconCarrierIdOverridesImpl
 // QTI_BEGIN: 2025-04-15: Android_UI: SystemUI: Readapt Mobile Icon Features For Kairos part 1
 import com.android.settingslib.mobile.MobileMappings
 // QTI_END: 2025-04-15: Android_UI: SystemUI: Readapt Mobile Icon Features For Kairos part 1
+import com.android.systemui.common.coroutine.ConflatedCallbackFlow.conflatedCallbackFlow
 import com.android.systemui.dagger.qualifiers.Background
+import com.android.systemui.Dependency
 import com.android.systemui.log.table.TableLogBuffer
 import com.android.systemui.log.table.logDiffsForTable
 import com.android.systemui.statusbar.pipeline.mobile.data.model.DataConnectionState.Connected
@@ -63,7 +66,9 @@ import com.android.systemui.statusbar.pipeline.shared.data.model.DataActivityMod
 import com.android.systemui.statusbar.policy.FiveGServiceClient.FiveGServiceState
 import com.android.systemui.util.CarrierNameCustomization
 // QTI_END: 2025-04-15: Android_UI: SystemUI: Readapt Mobile Icon Features For Kairos part 1
+import com.android.systemui.tuner.TunerService
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -219,6 +224,30 @@ class MobileIconInteractorImpl(
     override val isDataEnabled: StateFlow<Boolean> = connectionRepository.dataEnabled
     override val carrierNetworkChangeActive: StateFlow<Boolean> =
         connectionRepository.carrierNetworkChangeActive
+
+    private final val DATA_DISABLED_ICON: String =
+            "system:" + Settings.System.DATA_DISABLED_ICON;
+
+    private val shouldShowExclamationMark: StateFlow<Boolean> =
+        conflatedCallbackFlow {
+                val callback =
+                    object : TunerService.Tunable {
+                        override fun onTuningChanged(key: String, newValue: String?) {
+                            when (key) {
+                                DATA_DISABLED_ICON -> 
+                                    trySend(TunerService.parseIntegerSwitch(newValue, true))
+                            }
+                        }
+                    }
+                Dependency.get(TunerService::class.java).addTunable(callback, DATA_DISABLED_ICON)
+                awaitClose { Dependency.get(TunerService::class.java).removeTunable(callback) }
+            }
+            .stateIn(
+                scope,
+                started = SharingStarted.WhileSubscribed(),
+                true
+            )
+
     // True if there exists _any_ icon override for this carrierId. Note that overrides can include
     // any or none of the icon groups defined in MobileMappings, so we still need to check on a
     // per-network-type basis whether or not the given icon group is overridden
@@ -591,13 +620,15 @@ class MobileIconInteractorImpl(
                 isDataConnected,
                 isConnectionFailed,
                 isInService,
-                hideNoInternetState
-            ) { isDataEnabled, isDataConnected, isConnectionFailed, isInService,
-                        hideNoInternetState ->
-                !hideNoInternetState && (!isDataEnabled || (isDataConnected && isConnectionFailed)
-                        || !isInService)
+                hideNoInternetState,
+        ) { isDataEnabledVal, isDataConnectedVal, isConnectionFailedVal, isInServiceVal,
+                    hideNoInternetStateVal ->
+            !hideNoInternetStateVal && (!isDataEnabledVal || (isDataConnectedVal && isConnectionFailedVal)
+                    || !isInServiceVal)
+        }.combine(shouldShowExclamationMark) { showMark, shouldShow ->
+            showMark && shouldShow
 // QTI_END: 2025-04-15: Android_UI: SystemUI: Readapt Mobile Icon Features For Kairos part 1
-            }
+        }
             .stateIn(scope, SharingStarted.WhileSubscribed(), true)
     private val cellularShownLevel: StateFlow<Int> =
         combine(level, isInService, connectionRepository.inflateSignalStrength) {
