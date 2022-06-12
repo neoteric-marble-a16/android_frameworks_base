9decee079532cc40a9425402e478ecd860a5f1d4
HELLBOY017 <abhaygill017@gmail.com>
2022-06-12 17:42:59 +0530

    base: Add switch for small landscape notifications [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6724,6 +6724,12 @@ public final class Settings {
         public static final String SECURE_LOCKSCREEN_QS_DISABLED = "secure_lockscreen_qs_disabled";
 
         /**
+         * Whether to enable small size landcape notifications or not
+         * @hide
+         */
+        public static final String SMALL_LANDSCAPE_NOTIFICATION = "small_landscape_notification";
+
+	/**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -130,7 +130,8 @@ public class SystemSettings {
                 Settings.System.THREE_FINGER_GESTURE,
                 Settings.System.VOLBTN_MUSIC_CONTROLS,
 		        Settings.System.TORCH_POWER_BUTTON_GESTURE,
-		        Settings.System.ADVANCED_REBOOT
+		        Settings.System.ADVANCED_REBOOT,
+        	    Settings.System.SMALL_LANDSCAPE_NOTIFICATION
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
