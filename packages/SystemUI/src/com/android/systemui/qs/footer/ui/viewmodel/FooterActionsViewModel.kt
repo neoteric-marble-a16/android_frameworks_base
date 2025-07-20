e84e1a2f2cd14ca0d772101db2bb127c98a756b8
NurKeinNeid <nurkeinneid@derpfest.org>
2025-07-20 17:27:24 +0200

    SystemUI: Integrate data usage view model into footer actions
    * Added FooterActionsDataUsageViewModel to manage data usage display
    * Updated various components to utilize the new data usage model
    * Updated BuildNumber composable to use combinedClickable for interactions
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
@@ -77,6 +77,9 @@ class FooterActionsViewModel(
     val power: Flow<FooterActionsButtonViewModel?>,
     val initialPower: () -> FooterActionsButtonViewModel?,
 
+    /** The model for the data usage display. */
+    val dataUsage: FooterActionsDataUsageViewModel?,
+
     /**
      * Observe the device monitoring dialog requests and show the dialog accordingly. This function
      * will suspend indefinitely and will need to be cancelled to stop observing.
@@ -122,6 +125,7 @@ class FooterActionsViewModel(
         private val globalActionsDialogLiteProvider: Provider<GlobalActionsDialogLite>,
         private val activityStarter: ActivityStarter,
         @Named(PM_LITE_ENABLED) private val showPowerButton: Boolean,
+        private val dataUsageViewModel: FooterActionsDataUsageViewModel
     ) {
         /** Create a [FooterActionsViewModel] bound to the lifecycle of [lifecycleOwner]. */
         fun create(lifecycleOwner: LifecycleOwner): FooterActionsViewModel {
@@ -149,6 +153,7 @@ class FooterActionsViewModel(
                 globalActionsDialogLite,
                 activityStarter,
                 showPowerButton,
+                dataUsageViewModel,
             )
         }
 
@@ -174,6 +179,7 @@ class FooterActionsViewModel(
                 globalActionsDialogLite,
                 activityStarter,
                 showPowerButton,
+                dataUsageViewModel,
             )
         }
     }
@@ -187,6 +193,7 @@ fun createFooterActionsViewModel(
     globalActionsDialogLite: GlobalActionsDialogLite,
     activityStarter: ActivityStarter,
     showPowerButton: Boolean,
+    dataUsageViewModel: FooterActionsDataUsageViewModel? = null
 ): FooterActionsViewModel {
     suspend fun observeDeviceMonitoringDialogRequests(quickSettingsContext: Context) {
         footerActionsInteractor.deviceMonitoringDialogRequests.collect {
@@ -291,6 +298,7 @@ fun createFooterActionsViewModel(
         userSwitcher = userSwitcher,
         settings = settings,
         power = power,
+        dataUsage = dataUsageViewModel,
         observeDeviceMonitoringDialogRequests = ::observeDeviceMonitoringDialogRequests,
         initialPower =
             if (showPowerButton) {
