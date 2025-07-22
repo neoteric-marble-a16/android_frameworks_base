161e0c28dff16feee50968a99afd4c2b36b1e0d1
Abhay Singh Gill <abhaygill017@gmail.com>
2025-07-23 04:24:15 +0530

    SystemUI: Fixup footer actions color on UI mode changes
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/compose/features/src/com/android/systemui/qs/footer/ui/compose/FooterActions.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/qs/footer/ui/compose/FooterActions.kt
@@ -321,7 +321,7 @@ fun IconButton(
             ),
         useModifierBasedImplementation = useModifierBasedExpandable,
     ) {
-        val tint = model.iconTint?.let { Color(it) } ?: Color.Unspecified
+        val tint = model.iconTint?.let { colorAttr(it) } ?: Color.Unspecified
         Icon(model.icon, tint = tint, modifier = Modifier.size(20.dp))
     }
 }
@@ -408,7 +408,7 @@ private fun TextButton(
         shape = CircleShape,
         color = Color.Transparent,
         contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-        borderStroke = BorderStroke(1.dp, colorAttr(R.attr.shadeInactive)),
+        borderStroke = BorderStroke(1.dp, MaterialTheme.colorScheme.onSurfaceVariant),
         modifier =
             modifier
                 .padding(horizontal = 4.dp)
@@ -423,7 +423,7 @@ private fun TextButton(
             Icon(
                 icon,
                 Modifier.padding(end = 12.dp).size(20.dp),
-                colorAttr(R.attr.onShadeInactiveVariant),
+                MaterialTheme.colorScheme.onSurfaceVariant,
             )
 
             CompatText(
@@ -436,7 +436,7 @@ private fun TextButton(
                         MaterialTheme.typography.bodyMedium
                     },
                 letterSpacing = if (QsInCompose.isEnabled) 0.em else 0.01.em,
-                color = colorAttr(R.attr.onShadeInactiveVariant),
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
             )
@@ -450,7 +450,7 @@ private fun TextButton(
                     painterResource(com.android.internal.R.drawable.ic_chevron_end),
                     contentDescription = null,
                     Modifier.padding(start = 8.dp).size(20.dp),
-                    colorAttr(R.attr.onShadeInactiveVariant),
+                    MaterialTheme.colorScheme.onSurfaceVariant,
                 )
             }
         }
