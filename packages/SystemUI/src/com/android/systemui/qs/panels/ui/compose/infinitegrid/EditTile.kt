477e5e3109692f75757f859263d0df86abf14571
Abhay Singh Gill <abhaygill017@gmail.com>
2025-07-25 15:19:14 +0530

    SystemUI: Bring back dual target tile colors
    Well even though the A16 design is quite bad there should be something making us feel that we are not using A15.5
    
    Change-Id: Iffb7e7d91be9b9f656619156261120e763ccf044
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/EditTile.kt
@@ -1081,6 +1081,7 @@ private object EditModeTileDefaults {
     fun editTileColors(): TileColors =
         TileColors(
             background = LocalAndroidColorScheme.current.surfaceEffect2,
+            iconBackground = Color.Transparent,
             label = MaterialTheme.colorScheme.onSurface,
             secondaryLabel = MaterialTheme.colorScheme.onSurface,
             icon = MaterialTheme.colorScheme.onSurface,
