4e15498a034f3dd117c1afee92bcac2084afc1db
ShadeDark6 <nizamfa6@gmail.com>
2026-03-05 01:45:56 +0000

    Revert "PropImitationHooks: A temporary workaround..."
    This reverts commit e9ccdf6e5b7987b2f9d5f68fc6513551d00f5482.

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -44,7 +44,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -429,22 +428,21 @@ public class PropImitationHooks {
         }
     }
 
-    private static boolean isCallerPlayIntegrity() {
-        return Arrays.stream(Thread.currentThread().getStackTrace())
-                .map(StackTraceElement::getClassName)
-                .anyMatch(name -> name.toLowerCase(Locale.US).contains("droidguard"));
+    private static boolean isCallerSafetyNet() {
+        return sIsGms && Arrays.stream(Thread.currentThread().getStackTrace())
+                .anyMatch(elem -> elem.getClassName().contains("DroidGuard"));
     }
 
     public static void onEngineGetCertificateChain() {
         // If a keybox is found, don't block key attestation
-        /*if (KeyProviderManager.isKeyboxAvailable()) {
+        if (KeyProviderManager.isKeyboxAvailable()) {
             dlog("Key attestation blocking is disabled because a keybox is defined to spoof");
             return;
-        }*/
+        }
 
-        // Check stack for Play Integrity
-        if (isCallerPlayIntegrity()) {
-            dlog("Blocked key attestation for play integrity");
+        // Check stack for SafetyNet or Play Integrity
+        if (isCallerSafetyNet() || sIsFinsky) {
+            dlog("Blocked key attestation sIsGms=" + sIsGms + " sIsFinsky=" + sIsFinsky);
             throw new UnsupportedOperationException();
         }
     }
