edc283ee8bbd73abff0d8b36833fe034e74a5711
ShadeDark6 <nizamfa6@gmail.com>
2026-03-05 01:44:19 +0000

    Revert "KeyboxImitationHooks: Delete old keys if newer ones fail to generate"
    This reverts commit 18ece66358fe290d749eceb01b12164b5fd10bd4.

--- keystore/java/android/security/KeyStore2.java
+++ keystore/java/android/security/KeyStore2.java
@@ -33,7 +33,6 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.util.neoteric.KeyboxImitationHooks;
-import com.android.internal.util.neoteric.KeyboxUtils;
 
 import java.util.Calendar;
 
@@ -340,8 +339,6 @@ public class KeyStore2 {
     public void deleteKey(@NonNull KeyDescriptor descriptor)
             throws KeyStoreException {
         StrictMode.noteDiskWrite();
-
-        KeyboxUtils.remove(Binder.getCallingUid(), descriptor.alias);
 
         handleRemoteExceptionWithRetry((service) -> {
             service.deleteKey(descriptor);
