667f49db0ec5e67340e748040c2cab294da356ce
ShadeDark6 <nizamfa6@gmail.com>
2026-03-05 01:49:41 +0000

    Revert "PropImitationHooks: Spoof certified props to finsky as well"
    This reverts commit d5517b47f3c3b7995b1f1091c416a3cb8d52b57d.

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -249,8 +249,8 @@ public class PropImitationHooks {
          * Set Stock Fingerprint for ARCore
          * Set custom model for Netflix
          */
-        if (sIsGms || sIsFinsky) {
-            setPlayIntegrityProps(context);
+        if (sIsGms) {
+            setCertifiedPropsForGms(context);
         } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
             dlog("Setting stock fingerprint for: " + packageName);
             setPropValue("FINGERPRINT", sStockFp);
@@ -314,7 +314,7 @@ public class PropImitationHooks {
         }
     }
 
-    private static void setPlayIntegrityProps(Context context) {
+    private static void setCertifiedPropsForGms(Context context) {
         String savedProps = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.PIF_DATA);
         if (savedProps == null || TextUtils.isEmpty(savedProps)) {
             savedProps = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.FETCHED_PIF);
@@ -359,10 +359,10 @@ public class PropImitationHooks {
         };
 
         if (!was) {
-            dlog("Spoofing build for GMS / Finsky");
+            dlog("Spoofing build for GMS");
             setCertifiedProps();
         } else {
-            dlog("Skip spoofing build for GMS / Finsky, because GmsAddAccountActivityOnTop");
+            dlog("Skip spoofing build for GMS, because GmsAddAccountActivityOnTop");
         }
 
         try {
