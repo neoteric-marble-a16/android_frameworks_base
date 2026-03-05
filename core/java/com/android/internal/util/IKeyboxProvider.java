ebb877c9d11ba41c73c4b80d5aff8f2bbcb4b51d
ShadeDark6 <nizamfa6@gmail.com>
2026-03-05 01:59:34 +0000

    Revert "base: Add key generation for keybox spoofing [1/2]"
    This reverts commit e7884952ef3e5ce1da70dc5bda9f54e42e6967b6.

--- core/java/com/android/internal/util/IKeyboxProvider.java
+++ core/java/com/android/internal/util/IKeyboxProvider.java
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2024 Paranoid Android
  * SPDX-License-Identifier: Apache-2.0
  */
-package com.android.internal.util.neoteric;
+package com.android.internal.util;
 
 /**
  * Interface for keybox providers.
--- keystore/java/android/security/KeyStoreSecurityLevel.java
+++ keystore/java/android/security/KeyStoreSecurityLevel.java
@@ -33,8 +33,6 @@ import android.system.keystore2.KeyMetadata;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
-import com.android.internal.util.neoteric.KeyboxImitationHooks;
-
 import java.util.Calendar;
 import java.util.Collection;
 
@@ -147,15 +145,6 @@ public class KeyStoreSecurityLevel {
             Collection<KeyParameter> args, int flags, byte[] entropy)
             throws KeyStoreException {
         StrictMode.noteDiskWrite();
-
-        KeyboxImitationHooks.setSuccessFlag(false);
-        if (attestationKey == null) {
-            KeyMetadata metadata = KeyboxImitationHooks.generateKey(mSecurityLevel,
-                    descriptor, args);
-            if (metadata != null) {
-                return metadata;
-            }
-        }
 
         return handleExceptions(() -> mSecurityLevel.generateKey(
                 descriptor, attestationKey, args.toArray(new KeyParameter[args.size()]),
